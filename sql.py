f618cc3ff762d04c67d3f8bdc991da032daa28b7
Gadi Ezer <gadi.g.ezer@gmail.com>
2023-04-16 15:58:44 +0300

    made function not to exceed 30 lines of code

--- sql.py
+++ sql.py
@@ -75,51 +75,94 @@ def insert_product_into_db(products, curs):
     :return: None
     """
     for prod in products:
-        success = False
-        curs.execute(COUNT_PRODUCT_WITH_IHERB_ID.format(product_id=prod.product_id))
-        result = curs.fetchall()
-        num_result = result[0]['NUM_RESULT']
-        if num_result == 0:
-            try:
-                brand_name = '"' + prod.brand_name + '"'
-                curs.execute(SELECT_BRAND_WITH_NAME.format(brand_name=brand_name))
-                result = curs.fetchall()
-                brand_id = result[0]['id']
-                prod_name = prod.name.replace("'", " ")
-                curs.execute(INSERT_INTO_PRODUCT.format(product_id=prod.product_id, url=prod.url, prod_name=prod_name,
-                                                        rating=prod.rating, nb_reviews=prod.nb_reviews,
-                                                        part_no=prod.part_no, brand_id=brand_id,
-                                                        discount_price=prod.discount_price,
-                                                        out_of_stock=prod.out_of_stock,
-                                                        inventory_status=prod.inventory_status, currency=prod.currency,
-                                                        price=prod.price))
-                success = True
-            except pymysql.err.Error as e:
-                logging.info(f"""The product {str(prod)} has not been inserted into DB. Cause {e}""")
-        elif num_result == 1:
-            try:
-                curs.execute(
-                    UPDATE_PRODUCT.format(nb_reviews=prod.nb_reviews, rating=prod.rating, product_id=prod.product_id))
-                success = True
-            except pymysql.err.Error as e:
-                logging.info(
-                    f"""FAIL : {UPDATE_PRODUCT.format(nb_reviews=prod.nb_reviews, rating=prod.rating,
-                                                      product_id=prod.product_id)} CAUSE : {e}""")
-                print(f"UPDATE FAILED for {prod.product_id} !!!")
+        curs.execute(SELECT_PRODUCT_ID_USING_IHERB_ID.format(product_id=prod.product_id))
+        if curs.fetchone() is None:
+            create_new_product(prod, curs)
         else:
-            raise 'Houston we have a problem'
+            update_existing_product(prod, curs)
 
-        # If we were able to insert the product, we populate product_category table
-        if success:
-            curs.execute(SELECT_CATEGORY_ID.format(category=prod.category))
-            result = curs.fetchall()
-            category_id = result[0]['id']
-            curs.execute(SELECT_PRODUCT_ID_USING_IHERB_ID.format(product_id=prod.product_id))
-            result = curs.fetchall()
-            product_id = result[0]['id']
-            try:
-                curs.execute(INSERT_PRODUCT_CATEGORY.format(product_id=product_id,category_id=category_id))
-            except pymysql.err.Error as e:
-                logging.info(
-                    f"""FAIL : {INSERT_PRODUCT_CATEGORY.format(product_id=product_id,category_id=category_id)}  
-                    CAUSE = {e}""")
+        populate_product_category(prod, curs)
+
+
+def create_new_product(prod, curs):
+    """
+    Inserts a new product record into the database using the given product object and database cursor.
+
+    Args:
+        prod (Product): The product object to insert.
+        curs: The database cursor to use for executing SQL queries.
+
+    Returns:
+        None
+
+    Raises:
+        pymysql.err.Error: If there is an error executing the SQL query.
+    """
+    try:
+        brand_name = '"' + prod.brand_name + '"'
+        curs.execute(SELECT_BRAND_WITH_NAME.format(brand_name=brand_name))
+        result = curs.fetchall()
+        brand_id = result[0]['id']
+        prod_name = prod.name.replace("'", " ")
+        curs.execute(INSERT_INTO_PRODUCT.format(product_id=prod.product_id, url=prod.url, prod_name=prod_name,
+                                                rating=prod.rating, nb_reviews=prod.nb_reviews,
+                                                part_no=prod.part_no, brand_id=brand_id,
+                                                discount_price=prod.discount_price,
+                                                out_of_stock=prod.out_of_stock,
+                                                inventory_status=prod.inventory_status, currency=prod.currency,
+                                                price=prod.price))
+    except pymysql.err.Error as e:
+        logging.info(f"""The product {str(prod)} has not been inserted into DB. Cause {e}""")
+
+
+def update_existing_product(prod, curs):
+    """
+    Updates an existing product record in the database with the given product object and database cursor.
+
+    Args:
+        prod (Product): The product object to update.
+        curs: The database cursor to use for executing SQL queries.
+
+    Returns:
+        None
+
+    Raises:
+        pymysql.err.Error: If there is an error executing the SQL query.
+    """
+    try:
+        curs.execute(
+            UPDATE_PRODUCT.format(nb_reviews=prod.nb_reviews, rating=prod.rating, product_id=prod.product_id))
+    except pymysql.err.Error as e:
+        logging.info(
+            f"""FAIL : {UPDATE_PRODUCT.format(nb_reviews=prod.nb_reviews, rating=prod.rating,
+                                              product_id=prod.product_id)} CAUSE : {e}""")
+        print(f"UPDATE FAILED for {prod.product_id} !!!")
+
+
+def populate_product_category(prod, curs):
+    """
+    Inserts a new product-category association record into the database using the given product object and
+    database cursor.
+
+    Args:
+        prod (Product): The product object to associate with a category.
+        curs: The database cursor to use for executing SQL queries.
+
+    Returns:
+        None
+
+    Raises:
+        pymysql.err.Error: If there is an error executing the SQL query.
+    """
+    curs.execute(SELECT_CATEGORY_ID.format(category=prod.category))
+    result = curs.fetchall()
+    category_id = result[0]['id']
+    curs.execute(SELECT_PRODUCT_ID_USING_IHERB_ID.format(product_id=prod.product_id))
+    result = curs.fetchall()
+    product_id = result[0]['id']
+    try:
+        curs.execute(INSERT_PRODUCT_CATEGORY.format(product_id=product_id, category_id=category_id))
+    except pymysql.err.Error as e:
+        logging.info(
+            f"""FAIL : {INSERT_PRODUCT_CATEGORY.format(product_id=product_id, category_id=category_id)}
+                        CAUSE = {e}""")
