e53687a427f6b8ab16132f1471d50be231e254f0
Samuel B <bendavid.samuel@gmail.com>
2023-04-18 22:34:52 +0300

    simplify iherb.py

--- iherb.py
+++ iherb.py
@@ -33,24 +33,52 @@ DB_NAME = config["DB_NAME"]
 CATEGORIES = config["CATEGORIES"]
 DEFAULT_LIMIT = config["DEFAULT_LIMIT"]
 
-if __name__ == '__main__':
+def get_parameters_for_crapping():
+    """
+    Retrieve parameters for running a web scraping script.
+
+    Args:
+        None.
+
+    Returns:
+        A tuple containing two elements:
+        - A Namespace object containing the command-line parameters retrieved using argparse.
+        - The limit of results to retrieve, which can be either the value specified by the user or the default value.
+
+    Raises:
+        No exception is raised.
+
+    Description:
+        This function uses argparse to retrieve the command-line parameters required for running a web scraping script. The required parameters are:
+        - The category for which to retrieve the scraping results, which must be chosen from a predefined list of categories.
+        - (Optional) The maximum number of results to retrieve, which must be a positive integer.
+
+        If the user does not specify the limit parameter, the default limit will be used.
+
+        The function returns a tuple containing two elements:
+        - A Namespace object containing the command-line parameters retrieved using argparse.
+        - The limit of results to retrieve, which can be either the value specified by the user or the default value.
+
+        This function does not raise any exception.
+    """
     parser = argparse.ArgumentParser(description='Take a query')
     parser.add_argument('-c', '--category', type=str, metavar='', required=True, choices=config["CATEGORIES"],
                         help=f'Choose category from the following list:\n\n{config["CATEGORIES"]}')
     parser.add_argument('-l', '--limit', type=int, metavar='', help="(optional) number of results")
-    args = parser.parse_args()
-    if args.limit is None:
-        limit = DEFAULT_LIMIT
+    arguments = parser.parse_args()
+    if arguments.limit is None:
+        lim = DEFAULT_LIMIT
     else:
-        limit = args.limit
+        lim = arguments.limit
+    return arguments, lim
 
+if __name__ == '__main__':
+    args, limit = get_parameters_for_crapping()
     # Create an object Request_iherb
     req = requestiherb.RequestIherb(URL + args.category, limit)
     print(f"This request contains {min(limit, len(req.url_list))} pages of products")
-
     # Get the html of all the pages obtained with the request
     all_pages = requestiherb.get_html(req.url_list, limit)
-
     # Create a list of products from the pages obtained with the request
     for i, page in enumerate(all_pages):
         print(f"process the page : {i}")
